2808345dc0fb89e0b1e067a256b0cc75820f5a41
Jeremie Bornais <jeremiedevelops@gmail.com>
2023-09-26 12:30:20 +0000

    add new resume for insurance companies

--- src/Education.svelte
+++ src/Education.svelte
@@ -3,8 +3,8 @@
 
     const getEdu = async () => {
             let res = await fetch("./education.json");
-            let edu = await res.json();
-            return edu;
+            let edus = await res.json();
+            return edus;
         }
     const eduPromise = getEdu();
 </script>
@@ -14,19 +14,26 @@
     <Title name="Education"/>
     {#await eduPromise}
         <p>Loading education...</p>
-    {:then edu} 
-    <h3 class="primary-heading">{edu.degree}</h3>
-    <h4 class="secondary-heading">{edu.school}</h4>
-    <div class="icon-row" style="width: 100%;">
-        <span>
-            <i class="fa fa-calendar"></i>
-            {edu.duration}
-        </span>
-    </div>
-    <ul>
-        {#each edu.points as point}
-        <li class="point-text">- {@html point.text}</li>
-        {/each}
-    </ul>        
+    {:then edus}
+        {#each edus as edu}
+        <div class="education">
+            <h3 class="primary-heading">{edu.degree}</h3>
+            <h4 class="secondary-heading">{edu.school}</h4>
+            <div class="icon-row" style="width: 100%;">
+                <span>
+                    <i class="fa fa-calendar"></i>
+                    {edu.duration}
+                </span>
+            </div>
+            <ul>
+                {#each edu.points as point}
+                <li class="point-text">- {@html point.text}</li>
+                {/each}
+            </ul>   
+        </div>
+        {#if edu != edus[edus.length - 1]}
+        <div class="dotted-separator"></div>            
+        {/if}
+        {/each}     
     {/await}
 </div>
